c3f4acb265be04e12222fc2699a2d8ebfd4d9895
Deepak Bhalla <deepak.dbhalla@gmail.com>
2022-09-12 13:48:55 +0530

    Initial Commit

---
+++ src/main/java/com/example/springbootfileuploadservice/SpringbootFileUploadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfileuploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFileUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFileUploadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootfileuploadservice/SpringbootFileUploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootfileuploadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFileUploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
